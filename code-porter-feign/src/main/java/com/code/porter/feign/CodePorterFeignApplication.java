291f32adc215004ffbe7205682cc139d103b3d89
zhoubing <1992412092@qq.com>
2019-08-26 14:21:56 +0800

    update package name

---
+++ code-porter-feign/src/main/java/com/code/porter/feign/CodePorterFeignApplication.java
@@ -0,0 +1,13 @@
+package com.code.porter.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePorterFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodePorterFeignApplication.class, args);
+    }
+
+}
