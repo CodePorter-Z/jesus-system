7ee2c8fceac12e81b8b05a035a3b0121581f0843
zhoubing <1992412092@qq.com>
2019-08-26 14:05:51 +0800

    update project name

---
+++ code-porter-common/src/main/java/com/jesus/common/base/expant/dao/BaseDao.java
@@ -0,0 +1,19 @@
+package com.jesus.common.base.expant.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.io.Serializable;
+import java.util.List;
+
+@NoRepositoryBean
+public interface BaseDao<T,ID extends Serializable> extends JpaRepository<T, ID>, JpaSpecificationExecutor<T> {
+
+    /**
+     * 自定义查询
+     * @param sql 查询执行sql语句
+     * @return
+     */
+    public List<Object[]> selectBySQL(String sql);
+}
